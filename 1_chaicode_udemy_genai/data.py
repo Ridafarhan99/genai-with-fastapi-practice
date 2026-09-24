48e6888551f42187c1196ec2fe42c108de9fe99f
Rida Farhan <42339599+Ridafarhan99@users.noreply.github.com>
2026-09-24 21:52:54 +0530

    feat: new data added separation of conserns

---
+++ 1_chaicode_udemy_genai/data.py
@@ -0,0 +1,19 @@
+# separation of concerns
+menu_item = [
+    {
+        "id": 1,
+        "name": "Masala Chai",
+        "category": "Chai",
+        "description": "Indian Tea",
+        "price": 20,
+        "available": True,
+    },
+    {
+        "id": 1,
+        "name": "Masala Chai",
+        "category": "Chai",
+        "description": "Indian Tea",
+        "price": 20,
+        "available": True,
+    },
+]
